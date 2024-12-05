2e2e316af83da82cea9cf3de7fa58a44cc6f3185
HoangDucBach <hoangbach0985@gmail.com>
2024-12-05 10:56:06 +0700

    add: deploy scripts

---
+++ contracts/scripts/deploy.ts
@@ -0,0 +1,30 @@
+import { ethers } from "hardhat";
+
+async function main() {
+    try {
+        const PayChunkRegistry = await ethers.getContractFactory("PayChunkRegistry");
+        const payChunkRegistry = await PayChunkRegistry.deploy();
+        await payChunkRegistry.waitForDeployment();
+        const tx = payChunkRegistry.deploymentTransaction();
+
+        console.log("PayChunkRegistry deployed to:", await payChunkRegistry.getAddress());
+        console.log("Transaction hash:", tx?.hash);
+
+        const DynamicInvoiceTokenFactory = await ethers.getContractFactory("DynamicInvoiceTokenFactory");
+        const dynamicInvoiceTokenFactory = await DynamicInvoiceTokenFactory.deploy(payChunkRegistry.getAddress());
+        await dynamicInvoiceTokenFactory.waitForDeployment();
+        const tx2 = dynamicInvoiceTokenFactory.deploymentTransaction();
+
+        console.log("DynamicInvoiceTokenFactory deployed to:", await dynamicInvoiceTokenFactory.getAddress());
+        console.log("Transaction hash:", tx2?.hash);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
